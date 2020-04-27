9be621f932cf5bbc8b7c9baaf3eb51a86be26fd0
Bo Liu <bliu0201@gmail.com>
2020-04-26 19:23:32 -0500

    init

---
+++ agent.py
@@ -0,0 +1,200 @@
+import numpy as np
+import quadprog
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+
+from torch.optim import *
+from tqdm import tqdm
+
+
+class Replay(data.Dataset):
+    """
+    A dataset wrapper used as a memory to store the data
+    """
+    def __init__(self):
+        super(Replay, self).__init__()
+        self.rb = []
+
+    def __len__(self):
+        return len(self.rb)
+
+    def add(self, v, w, l, n_mem):
+        i = 0
+        while len(self.rb) < n_mem and i < v.shape[0]:
+            self.rb.append((v[i], w[i], l[i]))
+            i += 1
+
+    def sample(self, n):
+        batch = random.sample(self.rb, n)
+        return map(torch.stack, zip(*batch))
+
+    def reduce(self, m):
+        self.rb = self.rb[:m]
+
+
+class Model(nn.Module):
+    def __init__(self, dim_input):
+        super(Model, self).__init__()
+        dim_hidden = 128
+        self.base = nn.Sequential(
+            nn.Linear(dim_input, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, dim_hidden),
+            nn.ReLU(),
+        )
+        self.v_head = nn.Linear(dim_hidden, 1)
+        self.w_head = nn.Linear(dim_hidden, 1)
+
+    def forward(self, x):
+        h = self.base(x)
+        v, w = self.v_head(h), self.w_head(h)
+        return v, w
+
+
+class Agent(nn.Module):
+    """
+    using gradient episodic memory
+    """
+    def __init__(self,
+                 dim_input,
+                 mem_budget=500):
+        super(Agent, self).__init__()
+        self.task = -1
+        self.memories = {} # per task replay
+        self.n_mem = mem_budget
+        self.tqdm = True
+
+        self.net = Model(dim_input)
+        self.net.cuda()
+        self.create_optimizer()
+
+    def count_params(self):
+        n_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        return n_params
+
+    def increment_task(self):
+        self.task += 1
+        self.memories[self.task] = Replay()
+        self.create_optimizer()
+
+    def create_optimizer(self):
+        params = list(self.net.parameters())
+        self.opt = SGD(params, lr=1e-2)
+
+    def loss_fn(self, v, w, l):
+        vv, ww = self.net(l)
+        return F.mse_loss(vv, v) + F.mse_loss(ww, w)
+
+    def grad_to_vector(self):
+        vec = []
+        for n, p in self.named_parameters():
+            if 'weight' in n and p.grad is not None:
+                vec.append(p.grad.data.view(-1).clone())
+            else:
+                vec.append(torch.zeros_like(p).view(-1))
+        return torch.cat(vec)
+
+    def vector_to_grad(self, vec):
+        pointer = 0
+        for n, p in self.named_parameters():
+            num_param = p.numel()
+            if 'weight' in n and p.grad is not None:
+                p.grad.copy_(vec[pointer:pointer + num_param].view_as(p))
+            pointer += num_param
+
+    def project2cone2(self, gradient, memories):
+        memories_np = memories.cpu().double().numpy()
+        gradient_np = gradient.cpu().contiguous().view(-1).double().numpy()
+        t = memories_np.shape[0]
+
+        P = np.dot(memories_np, memories_np.transpose())
+        P = 0.5 * (P + P.transpose()) + np.eye(t) * 1e-3
+        q = np.dot(memories_np, gradient_np) * -1
+        G = np.eye(t)
+        h = np.zeros(t) + 0.5
+        v = quadprog.solve_qp(P, q, G, h)[0]
+        x = np.dot(v, memories_np) + gradient_np
+        x = torch.Tensor(x).view(-1, 1).cuda()
+        return x
+
+    def get_G(self):
+        prev_grads = []
+        for t, m in self.memories.items():
+            if t < self.task:
+                self.zero_grad()
+                v, w, l = m.sample(min(32, self.n_mem))
+
+                v = v.cuda().unsqueeze(-1)
+                w = w.cuda().unsqueeze(-1)
+                l = l.cuda()
+
+                loss = self.loss_fn(v, w, l)
+                loss.backward()
+                prev_grads.append(self.grad_to_vector())
+        return torch.stack(prev_grads)
+
+    def test(self, dataloader):
+        with torch.no_grad():
+            L = 0
+            for v, w, l in dataloader:
+                v = v.cuda().unsqueeze(-1)
+                w = w.cuda().unsqueeze(-1)
+                l = l.cuda()
+                loss = self.loss_fn(v, w, l)
+                L += loss.item()
+        print(f"[info] test loss is {L/len(dataloader)}")
+
+    def learn(self, dataloader, n_batches=500):
+        curr_mem = self.memories[self.task]
+
+        batch = 0
+        done = False
+        total_loss = 0
+
+        if self.tqdm:
+            pbar = tqdm(total=n_batches)
+
+        while not done:
+            for v, w, l in dataloader:
+                batch += 1
+
+                if len(curr_mem) < self.n_mem:
+                    curr_mem.add(v, w, l, self.n_mem)
+
+                v = v.cuda().unsqueeze(-1)
+                w = w.cuda().unsqueeze(-1)
+                l = l.cuda()
+
+                if self.task > 0:
+                    G = self.get_G()
+
+                self.opt.zero_grad()
+                loss = self.loss_fn(v, w, l)
+                loss.backward()
+                total_loss += (loss.detach().cpu().item() - total_loss) * 1/batch
+
+                if self.task > 0:
+                    curr_grad = self.grad_to_vector()
+                    dotp = G.mm(curr_grad.view(-1, 1))
+                    if (dotp < 0).sum() != 0:
+                        new_grad = self.project2cone2(curr_grad, G)
+                        self.vector_to_grad(new_grad)
+                self.opt.step()
+
+                if self.tqdm:
+                    pbar.set_description("mse error: %7.4f" % (total_loss))
+                    pbar.update(1)
+
+                if batch >= n_batches:
+                    done = True
+                    break
+
+        if self.tqdm:
+            pbar.close()
+
+        return total_loss
---
+++ main.py
@@ -0,0 +1,92 @@
+import csv
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from agent import *
+
+def make_data():
+    data = []
+    for task in range(1, 2+1):
+        csvf = f'./data/task{task}-lidar.csv'
+        with open (csvf, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            T0, LIDAR = [], []
+            for i, line in enumerate(reader):
+                if i == 0:
+                    continue
+                t = float(line[0])
+                lidar = [float(x) for x in line[14][1:-1].split(",")]
+                lidar = [(min(x, 10)-5)/5 for x in lidar]
+                T0.append(t)
+                LIDAR.append(lidar)
+
+        csvf = f'./data/task{task}-cmd.csv'
+        with open (csvf, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            T, V, W = [], [], []
+            for i, line in enumerate(reader):
+                if i == 0:
+                    continue
+                t = line[0]
+                v = line[2]
+                w = line[8]
+                t, v, w = map(float, [t, v, w])
+                T.append(t)
+                V.append(v)
+                W.append(w)
+
+        L = []
+        i=0
+        for j, (t, v, w) in enumerate(zip(T, V, W)):
+            while T0[i] < t:
+                i += 1
+            L.append(LIDAR[i-1])
+        V, W, L = map(np.array, [V, W, L])
+        data.append((V, W, L))
+    np.savez("./data/lifelong", data=data)
+
+class NavDataset(Dataset):
+    def __init__(self, V, W, L):
+        self.V = V.astype(np.float32)
+        self.W = W.astype(np.float32)
+        self.L = L.astype(np.float32)
+        self.n = V.shape[0]
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, idx):
+        v = self.V[idx]
+        w = self.W[idx]
+        l = self.L[idx]
+        return v, w, l
+
+def lifelong():
+    data = np.load("./data/lifelong.npz", allow_pickle=True)['data']
+
+    V1, W1, L1 = data[0]
+    V2, W2, L2 = data[1]
+
+    batch_size = 32
+    loader1 = torch.utils.data.DataLoader(NavDataset(V1, W1, L1), batch_size=batch_size, num_workers=4, shuffle=True)
+    loader2 = torch.utils.data.DataLoader(NavDataset(V2, W2, L2), batch_size=batch_size, num_workers=4, shuffle=True)
+
+    agent = Agent(dim_input=L1.shape[1])
+
+    print("[info] learning task 1 ...")
+    agent.increment_task()
+    agent.learn(loader1)
+    agent.test(loader1)
+
+    print("[info] learning task 2 ...")
+    agent.increment_task()
+    agent.learn(loader2)
+    agent.test(loader2)
+
+    agent.test(loader1)
+
+if __name__ == "__main__":
+    #make_data()
+    lifelong()
